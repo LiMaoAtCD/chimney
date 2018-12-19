121ed91292183a693359701ef95b91cb1dab8b7f
limao <limitscofield@gmail.com>
2018-12-19 16:05:55 +0800

    json

--- lib/firstpage/firstpage.dart
+++ lib/firstpage/firstpage.dart
@@ -1,38 +1,48 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:dio/dio.dart';
 
 class FirstPage extends StatefulWidget {
-  FirstPage({this.title}): super(key: UniqueKey());
+  FirstPage({this.title}) : super(key: UniqueKey());
   final String title;
   _FirstPageState createState() => _FirstPageState();
-
 }
 
 class _FirstPageState extends State<FirstPage> {
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  Future fetchData() async {
+    Options ops = Options(
+      baseUrl: "http://v.juhe.cn/",
+      connectTimeout: 3000,
+      receiveTimeout: 5000,
+    );
+
+    var dio = Dio(ops);
+    Response res = await dio.get("historyWeather/weather", data: {
+      "key": "9795025aa92ce94c97a4404d98d94a9e",
+      "city_id": 1,
+      "weather_date": "2018-01-01"
+    });
+    print(res.data.toString());
+  }
 
   @override
-    void initState() {
-      print('initstate is called');
-      super.initState();
-    }
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    print("didChangeDependencies");
+  }
 
-    @override
-      void didChangeDependencies() {
-        print('didChangeDependencies');
-        super.didChangeDependencies();
-      }
+  @override
+  void dispose() {
+    print('dispose');
+    super.dispose();
+  }
 
-      @override
-        void deactivate() {
-          print('deactivate');
-          super.deactivate();
-        }
-
-        @override
-          void dispose() {
-            print('dispose');
-            super.dispose();
-          }
-          
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,3 +60,5 @@ class _FirstPageState extends State<FirstPage> {
     );
   }
 }
+
+
