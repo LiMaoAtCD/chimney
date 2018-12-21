796110dd9c782f4f34983e55923d215f7015a205
AlienLi <liamscofield@icloud.com>
2018-12-21 22:16:14 +0800

    bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:chimney/tabbar/tab.dart';
+import 'package:bloc/bloc.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    BlocSupervisor().delegate = SimpleBlocDelegate();
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -12,5 +15,12 @@ class MyApp extends StatelessWidget {
       ),
       home: ChimneyTabbar(),
     );
+  }
+}
+
+class SimpleBlocDelegate extends BlocDelegate {
+  @override
+  void onTransition(Transition transition) {
+    print(transition.toString());
   }
 }
--- lib/second/bloc.dart
+++ lib/second/bloc.dart
@@ -1,15 +1,81 @@
 import 'package:flutter/material.dart';
-
+import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'dart:async';
 
 class Counter extends StatefulWidget {
   _CounterState createState() => _CounterState();
 }
 
 class _CounterState extends State<Counter> {
+  final CounterBloc _counterBloc = CounterBloc();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-       child: Container(),
+    return BlocProvider<CounterBloc>(
+      bloc: _counterBloc,
+      child: CounterPage(),
     );
   }
+}
+
+class CounterPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final CounterBloc _countBloc = BlocProvider.of<CounterBloc>(context);
+    return Column(
+      children: <Widget>[
+        BlocBuilder<CounterEvent, int>(
+          bloc: _countBloc,
+          builder: (BuildContext context, int count) {
+            return Center(child: Text('$count', style: TextStyle(fontSize: 24.0,),),);
+          },
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: RaisedButton(
+            onPressed: (){
+              _countBloc.dispatch(Increment());
+            },
+          ),
+        ),
+          Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: RaisedButton(
+            onPressed: (){
+              _countBloc.dispatch(Decrement());
+            },
+          ),
+        ),
+
+      ],
+    );
+  }
+}
+
+abstract class  CounterEvent {
+
+}
+
+class Increment extends CounterEvent {
+  String toString() => 'Increment';
+}
+
+class Decrement extends CounterEvent {
+  String toString() => 'Decrement';
+}
+
+class CounterBloc extends Bloc<CounterEvent, int> {
+  @override
+    int get initialState => 0;
+
+  @override
+  Stream<int> mapEventToState(int currentState, CounterEvent event) async* {
+    if (event is Increment) {
+      yield currentState + 1;
+    } 
+    if (event is Decrement) {
+      yield currentState - 1;
+    }
+  } 
 }
